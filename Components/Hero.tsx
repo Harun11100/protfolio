c1b90edc7a7c1b76e8835d9be752ff2dea63973f
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-06-13 01:27:08 +0600

    Bentogrid added

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -15,9 +15,9 @@ const Hero = () => {
       <Spotlight className='-top-27 left-80
       h-[80vh] w-[50vh]' fill='blue'/>
       
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.4] bg-grid-black/[0.3]  flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.1]  flex items-center justify-center absolute top-0 left-0">
      
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black)]"/>
       
     </div>
     <div className=' flex justify-center relative my-20 z-10' >
@@ -28,7 +28,7 @@ const Hero = () => {
             <TextGenerateEffect 
             className='text-center text-[40px] 
             md:text-5xl lg:text-6xl'
-            words='Transform Concepts into Seamless Experiences'/>
+            words='Transform Concepts into Seamless User Experiences '/>
             
 
             <p className='text-center md:tracking-wider mb-5
